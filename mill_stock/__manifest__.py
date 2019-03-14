39ad99b9794404680d727dc65475b4f4ae68aebd
Shivam Goyal <shivam1111@gmail.com>
2019-03-14 11:22:51 +0530

    removed bug

--- mill_stock/__manifest__.py
+++ mill_stock/__manifest__.py
@@ -14,7 +14,6 @@
     'depends': ['base','mail','mill_order','chemical_compositions','mill_purchase_order'],
     'data': [
         'data/ir_sequence_data.xml',
-        'views/mill_stock_view.xml',
         'views/material_grade_view.xml',
         'views/mill_purchase_order_view.xml',
         'wizard/mill_stock_wizard_view.xml',
@@ -22,6 +21,7 @@
         'security/ir.model.access.csv',
         'report/heat_report.xml',
         'views/heat_view.xml',
+        'views/mill_stock_view.xml',
     ],
     'demo': [
     ],
